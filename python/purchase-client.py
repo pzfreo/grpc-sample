b323600a904235252f52a21df67258bfe106bfb3
Oxford Student <pzfreo@github.com>
2018-01-10 18:54:22 +0000

    .

---
+++ python/purchase-client.py
@@ -0,0 +1,18 @@
+import grpc;
+import purchase_pb2;
+import purchase_pb2_grpc;
+import time;
+
+def run():
+  channel = grpc.insecure_channel('localhost:50051')
+  stub = purchase_pb2_grpc.PurchaseStub(channel)
+  print ("starting")
+
+  t0 = time.time();
+  for i in range(1,1000):
+      response = stub.purchase(purchase_pb2.PurchaseRequest(poNumber="001",quantity=5))
+  print str(time.time() - t0)+" secs per thousand requests"
+  print("Purchase client received: " + response.message)
+  print("Purchase client received: " + str(response.returncode))
+
+run()
