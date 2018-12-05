c6c2541bd53e681e1b989b6999cf9376bf56aa83
Oxford Student <pzfreo@github.com>
2018-12-05 14:42:01 +0000

    .

--- answer/python/purchase-client.py
+++ answer/python/purchase-client.py
@@ -9,9 +9,9 @@ def run():
   print ("starting")
 
   t0 = time.time();
-  for i in range(1,1000):
+  for i in range(1,10000):
       response = stub.purchase(purchase_pb2.PurchaseRequest(poNumber="001",quantity=5))
-  print str(time.time() - t0)+" secs per thousand requests"
+  print str(10000/(time.time() - t0))+" requests / sec"
   print("Purchase client received: " + response.message)
   print("Purchase client received: " + str(response.returncode))
 
